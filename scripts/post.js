f8da84dee26ff75c997c71fc08bbd05839ce0491
Sepand Ansari <sepans@sepans.com>
2016-01-07 17:59:27 -0500

    greeting msg

--- scripts/post.js
+++ scripts/post.js
@@ -2,3 +2,22 @@ $('.slides').slick({
 	dots: true,
 	slidesToShow: 1
 });
+
+var greetings = ['Greetings', 'Good morning', 'Good afternoon', 'Good eveneing', 'Good night'];
+
+var date = new Date(),
+    greetingIndex = 0,
+    hour = date.getHours();
+
+if(hour>=0 && hour<12) {
+  greetingIndex = 1;
+} 
+else if(hour>=12 && hour<18) {
+  greetingIndex = 2;
+}
+else if(hour>=18 && hour<20) {
+  greetingIndex = 3;
+}
+
+$('.greeting').text(greetings[greetingIndex]);
+
