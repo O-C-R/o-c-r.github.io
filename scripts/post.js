44dae135f8ab9c5ade76d816b2231f1ab23057dd
Sepand Ansari <sepans@sepans.com>
2016-01-07 19:02:18 -0500

    typo and minor css

--- scripts/post.js
+++ scripts/post.js
@@ -3,19 +3,19 @@ $('.slides').slick({
 	slidesToShow: 1
 });
 
-var greetings = ['Greetings', 'Good morning', 'Good afternoon', 'Good eveneing', 'Good night'];
+var greetings = ['Greetings', 'Good morning', 'Good afternoon', 'Good evening', 'Good night'];
 
 var date = new Date(),
     greetingIndex = 0,
     hour = date.getHours();
 
-if(hour>=0 && hour<12) {
+if(hour>=4 && hour<12) {
   greetingIndex = 1;
 } 
 else if(hour>=12 && hour<18) {
   greetingIndex = 2;
 }
-else if(hour>=18 && hour<20) {
+else if(hour>=18 && hour<=23) {
   greetingIndex = 3;
 }
 
